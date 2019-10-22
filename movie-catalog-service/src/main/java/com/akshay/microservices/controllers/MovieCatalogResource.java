b32c898f4e0e9319d04bfd5487cd0a7b8a7fe817
Akshay Nande <akshaytnande@gmail.com>
2019-10-22 08:31:48 -0700

    Made a call to rating api and returned an object response type instead of list

--- movie-catalog-service/src/main/java/com/akshay/microservices/controllers/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/akshay/microservices/controllers/MovieCatalogResource.java
@@ -1,6 +1,5 @@
 package com.akshay.microservices.controllers;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -12,7 +11,7 @@ import org.springframework.web.client.RestTemplate;
 
 import com.akshay.microservices.models.CatalogItem;
 import com.akshay.microservices.models.Movie;
-import com.akshay.microservices.models.Rating;
+import com.akshay.microservices.models.UserRating;
 
 @RestController
 @RequestMapping("/catalog")
@@ -22,19 +21,18 @@ public class MovieCatalogResource {
 	private RestTemplate restTemplate;
 	
 	// get all rated movie IDs
-	// For each movie ID, call movie info service and get details
-	// Put them all together
-	
+
 	@RequestMapping("/{userId}")
 	public List <CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 		
-		List<Rating> ratings = Arrays.asList(
-				new Rating("1234", 4),
-				new Rating("2345", 3)
-				);
+		UserRating ratings = restTemplate.getForObject("http://localhost:8083/ratingsdata/users/" + userId, UserRating.class);
 	
-		return ratings.stream().map(rating -> { 
+		return ratings.getUserRating().stream().map(rating -> {
+			
+			// For each movie ID, call movie info service and get details
 			Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+			
+			// Put them all together
 			return new CatalogItem(movie.getName(), "part 1", rating.getRating());
 		})
 				.collect(Collectors.toList());
--- ratings-data-service/src/main/java/com/akshay/microservices/controllers/RatingController.java
+++ ratings-data-service/src/main/java/com/akshay/microservices/controllers/RatingController.java
@@ -1,10 +1,14 @@
 package com.akshay.microservices.controllers;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.akshay.microservices.controllers.models.Rating;
+import com.akshay.microservices.controllers.models.UserRating;
 
 @RestController
 @RequestMapping("/ratingsdata")
@@ -12,9 +16,20 @@ public class RatingController {
 	
 	@RequestMapping("/{movieId}")
 	public Rating getRating(@PathVariable("movieId") String movieId) {
-		
 		return new Rating(movieId, 4);
+	}
+	
+	@RequestMapping("users/{userId}")
+	public UserRating getUserRating(@PathVariable("userId") String userId) {
+		List<Rating> ratings = Arrays.asList(
+					new Rating("1234", 4),
+					new Rating("2345",3)
+				);
 		
+		UserRating userRating = new UserRating();
+		userRating.setUserRating(ratings);
+		
+		return userRating;
 	}
 
 }
